8f3c448502ea97eaae06469699048067a3591b1e
Larysa1387 <kaliuzhna.off@gmail.com>
2021-11-11 17:51:47 +0200

    add all components to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Balance from './components/Balance/Balance';
+import ExchangeRate from './components/ExchangeRate/ExchangeRate';
+import Navigation from './components/Navigation/Navigation';
+import SideBar from './components/SideBar';
+
 function App() {
-  return <div>Wallet</div>
+  return (
+    <>
+      <div>Wallet</div>
+      <SideBar>
+        <Navigation />
+        <Balance />
+        <ExchangeRate />
+      </SideBar>
+    </>
+  );
 }
 
-export default App
+export default App;
