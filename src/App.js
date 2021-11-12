498aef4ff94ded53949362909420d45068350681
Yauheni Luzakou <luzakou@yandex.ru>
2021-11-12 20:44:20 +0100

    add normalize, fonstm colors, redux

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import Balance from './components/Balance/Balance';
 import ExchangeRate from './components/ExchangeRate/ExchangeRate';
 import Navigation from './components/Navigation/Navigation';
 import SideBar from './components/SideBar';
+import GlobalStyle from './styles/normalize';
+import Fonts from './styles/fonts'
+import React from 'react';
 
 function App() {
   return (
     <>
+      <Fonts/>
+      <GlobalStyle rwdVersion/>
       <div>Wallet</div>
       <SideBar>
         <Navigation />
