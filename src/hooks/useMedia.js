e19afd47cc3ef609620bb378997079bc15fb4e1a
Yauheni Luzakou <luzakou@yandex.ru>
2021-11-14 20:11:03 +0100

    fix response size and add useMEdia

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 export const useMedia = (query) => {
   const [matches, setMatches] = useState(window.matchMedia(query).matches);
-
   useEffect(() => {
     let media = window.matchMedia(query); // < 500px
     if (media.matches !== matches) {
@@ -9,9 +8,7 @@ export const useMedia = (query) => {
     }
     let listener = () => setMatches(media.matches);
       media.addListener(listener);
-
     return () => media.removeListener(listener);
   }, [matches, query]);
-
   return matches;
 }
