97f9e91ca1d6b36152c8520e39104fbe5bfabbfd
Larysa1387 <kaliuzhna.off@gmail.com>
2021-11-17 01:09:57 +0200

    add background to the table

--- src/components/ExchangeRate/ExchangeRate.js
+++ src/components/ExchangeRate/ExchangeRate.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import fetchExchangeRate from '../services/exchangeRateAPI/exchangeRateAPI';
+import fetchExchangeRate from '../../services/exchangeRateAPI/exchangeRateAPI';
 // import { useDispatch } from 'react-redux';
 
 import { Wrapper, Table, TableHead, StyledTh, StyledTd } from './styledComp';
 
 const ExchangeRate = () => {
+  const [loading, setLoading] = useState(false);
   const [currencyArr, setCurrencyArr] = useState([]);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    fetchExchangeRate().then(currArr => {
-      currArr.pop();
-      setCurrencyArr(currArr);
-    });
+    setLoading(true);
+    fetchExchangeRate()
+      .then(currArr => {
+        currArr.pop();
+        setCurrencyArr(currArr);
+      })
+      .catch(error => setError(error))
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
   // console.log(currencyArr[0].buy.slice(0, 5));
   return (
@@ -23,7 +31,30 @@ const ExchangeRate = () => {
             <StyledTh>Продажа</StyledTh>
           </tr>
         </TableHead>
-
+        {error && (
+          <h1
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              color: 'white',
+            }}
+          >
+            {error.message} <br />
+            Reload the page
+          </h1>
+        )}
+        {loading && (
+          <p
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              color: 'white',
+              padding: '40px',
+            }}
+          >
+            Loading...
+          </p>
+        )}
         <tbody>
           {currencyArr.map(({ ccy, buy, sale }) => (
             <tr key={ccy}>
--- src/components/ExchangeRate/styledComp.js
+++ src/components/ExchangeRate/styledComp.js
@@ -1,21 +1,17 @@
 import styled from 'styled-components';
 import { breakpoint, breakpointMax } from '../../styles/breakpoint';
+const imgUrl = require('../../icons/background.svg').default;
 
 const Wrapper = styled.div`
-  /* width: 350px;
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 0.2) -7.46%,
-    rgba(255, 255, 255, 0) 100%
-  ); */
   margin-bottom: 0;
   ${breakpointMax.m`
-    display:none;
+    // display:none;
   `};
   ${breakpointMax.l`
   margin-top: 20px;
   `};
 `;
+
 const Table = styled.table`
   border-radius: 30px;
   font-size: 18px;
@@ -26,33 +22,39 @@ const Table = styled.table`
   border: 2px solid rgb(177, 172, 199);
   background-color: #4a56e2;
   box-shadow: 4px 4px 8px 0px rgb(0 0 0 / 30%);
-  ${breakpointMax.l`
-    width: 334px;
+  background-image: url(${imgUrl});
+  background-repeat: no-repeat;
+  background-position: bottom;
+  ${breakpoint.s`
+    width: 280px;
     height: 174px;
+    background-size: 280px 93px;
+  `};
+  ${breakpoint.m`
+    width: 334px;
+    background-size: 334px 111px;
   `};
   ${breakpoint.l`
     display: block;
     width: 350px;
     height: 347px;
     margin-bottom: 50px;
+    background-size: 350px 134px;
   `};
-  /* background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 0.2) -7.46%,
-    rgba(255, 255, 255, 0) 100%
-  ); */
 `;
+
 const TableHead = styled.thead`
-  background-color: cornflowerblue;
-  /* opacity: 0.2; */
-  /* letter-spacing: 1.7px; */
+  background-color: #626ce5;
   color: #fff;
 `;
 
 const StyledTh = styled.th`
   text-align: center;
-  ${breakpointMax.l`
+  ${breakpoint.s`
     padding: 10px;
+    // padding-left: 17px;
+  `};
+  ${breakpoint.m`
     padding-left: 20px;
   `};
   ${breakpoint.l`
@@ -66,7 +68,10 @@ const StyledTd = styled.td`
   line-height: 1.5;
   text-align: start;
   color: #fff;
-  ${breakpointMax.l`
+  ${breakpoint.s`
+    padding-left: 17px;
+  `};
+  ${breakpoint.m`
     padding: 10px;
     padding-left: 28px;
     text-align: start;
@@ -75,11 +80,6 @@ const StyledTd = styled.td`
     padding: 13px;
     padding-left: 30px;
   `};
-  /* background-color: #4a56e2; */
-  /* background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 0.2) -7.46%,
-    rgba(255, 255, 255, 0) 100%
-  ); */
 `;
+
 export { Wrapper, Table, TableHead, StyledTh, StyledTd };
