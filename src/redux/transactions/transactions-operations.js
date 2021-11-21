950b95eca54c0d7ca0655182a80b23c033da786c
Maryna1704 <74004216+Maryna1704@users.noreply.github.com>
2021-11-21 11:52:55 +0200

    new cr

--- src/redux/transactions/transactions-operations.js
+++ src/redux/transactions/transactions-operations.js
@@ -1,70 +1,85 @@
 import axios from 'axios';
-import { fetchTransactionRequest,
-    fetchTransactionSuccess,
-    fetchTransactionError,
-    addTransactionRequest,
-    addTransactionSuccess,
-    addTransactionError,
-    getQueryStatisticsRequest,
-    getQueryStatisticsSuccess,
-    getQueryStatisticsError,
-    getTransactionCategoriesRequest,
-    getTransactionCategoriesSuccess,
-    getTransactionCategoriesError
-    } from './transactions-actions'
+import {
+  fetchTransactionRequest,
+  fetchTransactionSuccess,
+  fetchTransactionError,
+  addTransactionRequest,
+  addTransactionSuccess,
+  addTransactionError,
+  getQueryStatisticsRequest,
+  getQueryStatisticsSuccess,
+  getQueryStatisticsError,
+  getTransactionCategoriesRequest,
+  getTransactionCategoriesSuccess,
+  getTransactionCategoriesError,
+} from './transactions-actions';
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-    //GET 
+//GET
 const fetchTransactions = () => async dispatch => {
-    dispatch(fetchTransactionRequest());
+  dispatch(fetchTransactionRequest());
 
-    try {
-        const { data } = await axios.get('/transactions');
+  try {
+    const { data } = await axios.get('/transactions');
 
-        dispatch(fetchTransactionSuccess(data.data.transactions));
-    } catch (error) {
-        dispatch(fetchTransactionError(error.message));
-    }
+    dispatch(fetchTransactionSuccess(data.data.transactions));
+  } catch (error) {
+    dispatch(fetchTransactionError(error.message));
+  }
 };
 
 //POST
 const addTransaction = transactionData => async dispatch => {
+  dispatch(addTransactionRequest());
 
-    dispatch(addTransactionRequest());
-
-    try {
-        const { data } = await axios.post('/transactions', transactionData);
-        console.log("data", data.data.result)
-        dispatch(addTransactionSuccess(data.data.result));
-        
-    } catch (error) {
+  try {
+    const { data } = await axios.post('/transactions', transactionData);
+    // console.log('data', data.data.result);
+    dispatch(addTransactionSuccess(data.data.result));
+  } catch (error) {
     dispatch(addTransactionError(error.message));
-    }
+    toast.error(' Недостаточно средств на счету', {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+  }
 };
 
 //GET
-const getQueryStatistics = ({ month, year }) => async dispatch => {
+const getQueryStatistics =
+  ({ month, year }) =>
+  async dispatch => {
     dispatch(getQueryStatisticsRequest());
 
     try {
-        const { data } = await axios.get(`/transactions/statistics?month=${month}&year=${year}`);
+      const { data } = await axios.get(
+        `/transactions/statistics?month=${month}&year=${year}`,
+      );
 
-        dispatch(getQueryStatisticsSuccess(data));
+      dispatch(getQueryStatisticsSuccess(data));
     } catch (error) {
-        dispatch(getQueryStatisticsError(error));
-        }
-    };
+      dispatch(getQueryStatisticsError(error));
+    }
+  };
 
 // getCategories
 const getCategories = () => async dispatch => {
-    dispatch(getTransactionCategoriesRequest());
+  dispatch(getTransactionCategoriesRequest());
 
-    try {
-        const { data } = await axios.get('/transactions/categories');
-        dispatch(getTransactionCategoriesSuccess(data.data));
-    } catch (error) {
-        dispatch(getTransactionCategoriesError(error.message));
-    }
+  try {
+    const { data } = await axios.get('/transactions/categories');
+    dispatch(getTransactionCategoriesSuccess(data.data));
+  } catch (error) {
+    dispatch(getTransactionCategoriesError(error.message));
+  }
 };
 
 export { fetchTransactions, addTransaction, getQueryStatistics, getCategories };
--- src/redux/transactions/transactions-selectors.js
+++ src/redux/transactions/transactions-selectors.js
@@ -1,16 +1,18 @@
 const getAllTransactions = state => state.transactions.allTransactions;
 const getAddTransactions = state => state.transactions.addTransactions;
 const getAllQueryStatistics = state => state.transactions.queryStatistics;
-const getIsModalAddTransactionOpen = state => state.transactions.modalTransaction;
-const getTransactionCategories = state => state.transactions.transactionCategories;
-
+const getIsModalAddTransactionOpen = state =>
+  state.transactions.modalTransaction;
+const getTransactionCategories = state =>
+  state.transactions.transactionCategories;
+const getCurrentBalance = state =>
+  state.transactions.allTransactions[0].balance;
 
 export {
   getAllTransactions,
   getAddTransactions,
   getAllQueryStatistics,
   getIsModalAddTransactionOpen,
-  getTransactionCategories
+  getTransactionCategories,
+  getCurrentBalance,
 };
-
-
